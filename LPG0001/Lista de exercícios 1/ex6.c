0754dc828802349fe9f59579d941b8dcaa5c0041
Davidjanovix <davidjanovix@gmail.com>
2025-04-01 16:57:06 -0300

    Atualização

---
+++ LPG0001/Lista de exercícios 1/ex6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float x, seno, frac = 1;
+    int i, n;
+    printf("Digite o valor de x: ");
+    scanf("%f", &x);
+    printf("Digite o numero de termos: ");
+    scanf("%d", &n);
+    for (i = 1 ; i <= n ; i++) {
+        seno += frac;
+        frac *= x
+    }
+    return 0;
+}
