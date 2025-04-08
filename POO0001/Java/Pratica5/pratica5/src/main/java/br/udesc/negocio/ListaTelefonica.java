701fb6a4d865be18f61b10123b1e8a06e99566f7
Davidjanovix <davidjanovix@gmail.com>
2025-04-08 15:42:36 -0300

    Update ListaTelefonica.java

--- POO0001/Java/Pratica5/pratica5/src/main/java/br/udesc/negocio/ListaTelefonica.java
+++ POO0001/Java/Pratica5/pratica5/src/main/java/br/udesc/negocio/ListaTelefonica.java
@@ -2,7 +2,6 @@ package br.udesc.negocio;
 import br.udesc.dados.*;
 import java.util.List;
 import java.util.LinkedList;
-//import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
